21445d6e45dcd18e1e355de7f227341f739444d7
shi-314 <zn31415926535@gmail.com>
2013-11-12 21:02:40 +0100

    added context menu plugin

--- js/seq.js
+++ js/seq.js
@@ -5,46 +5,98 @@
  * @date 30.10.13
  */
 
-(function(mme2) {
+(function (mme2) {
 
-    "use strict";
+	"use strict";
 
-    mme2.SequenceEditor = function(options) {
+	mme2.SequenceEditor = function (options) {
 
 		console.log("creating sequence editor...");
 
-		$('.sequenceEditor').append('<input type="text" id="seqInput">');
+		this.editor = $('.sequenceEditor');
+		this.editor.append('<input type="text" id="seqInput">');
 		this.seqInput = $('#seqInput');
+
+		this.selectedElement = null;
 
 		var _this = this;
 
 		$('.sequenceElement').drags();
 
-		$('.sequenceElement').dblclick(function(e) {
+		$('.sequenceElement').dblclick(function (e) {
 			_this.seqInput.val(e.target.innerText);
 			_this.showTextInput({x: e.clientX + 10, y: e.clientY - 40});
+			_this.select(e.target);
 		});
 
-		$('.sequenceEditor').click(function(e) {
+		this.editor.click(function (e) {
 			_this.hideTextInput();
+			_this.unselect();
 		});
 
-		this.seqInput.click(function(e) {
+		this.seqInput.click(function (e) {
 			e.stopPropagation();
 		});
 
-		this.seqInput.pressEnter(function() {
-			console.log("test");
+		this.seqInput.pressEnter(function () {
+			_this.selectedElement.text(_this.seqInput.val());
+			_this.hideTextInput();
 		});
 
-    }
+		//
+		// context menu for sequence elements
+		//
 
-	mme2.SequenceEditor.prototype.showTextInput = function(p) {
+		$('.sequenceElement').contextMenu('element-menu', {
+				'Connect': {
+					click: function (element) {
+						element.css({backgroundColor: 'pink'});
+					}
+				},
+				'Delete': {
+					click: function(element) {
+						element.detach();
+					}
+				}
+			}
+		);
+
+		//
+		// context menu for the editor
+		//
+
+		this.editor.contextMenu('element-menu', {
+				'New': {
+					click: function (element) {
+
+					}
+				},
+				'Clear': {
+					click: function (element) {
+
+					}
+				}
+			}
+		);
+	}
+
+	mme2.SequenceEditor.prototype.showTextInput = function (p) {
 		this.seqInput.fadeIn('fast').offset({left: p.x, top: p.y}).focus().select();
 	}
 
-	mme2.SequenceEditor.prototype.hideTextInput = function() {
+	mme2.SequenceEditor.prototype.hideTextInput = function () {
 		this.seqInput.fadeOut('fast');
+	}
+
+	mme2.SequenceEditor.prototype.select = function (e) {
+		this.unselect();
+		this.selectedElement = $(e);
+		this.selectedElement.addClass('selected');
+	}
+
+	mme2.SequenceEditor.prototype.unselect = function () {
+		if (this.selectedElement)
+			this.selectedElement.removeClass('selected');
 	}
 
 })(window.mme2 = window.mme2 || {});
